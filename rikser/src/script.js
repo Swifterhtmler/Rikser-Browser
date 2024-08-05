ddaa8f464427ea6d448b54833aa06a6fbca11346
Riku Kuisma <rikukuisma@icloud.com>
2024-08-06 00:57:16 +0300

    finished ui prod

--- rikser/src/script.js
+++ rikser/src/script.js
@@ -1,20 +1,29 @@
-const expect = function(val) {
-    return {
-        toBe: function(sval) {
-            if (val === sval) {
-                return true;
-            } else {
-                throw new Error("Not Equal");
-            }
-        },
-        notToBe: function(tval) {
-            if (val !== tval) {
-                return true;
-            } else {
-                throw new Error("Equal");
-            }
-        }
-    };
-};
 
-console.log(expect(5).toBe(5))
+
+const backButton = document.getElementById('back');
+const forwardButton = document.getElementById('forward');
+const resButton = document.getElementById('refresh');
+
+backButton.addEventListener('mouseover', () => {
+    backButton.style.boxShadow ='5px 5px 5px  rgba(141, 156, 164, 0.65)';
+});
+
+backButton.addEventListener('mouseout', () => {
+    backButton.style.boxShadow ='none';
+});
+
+forwardButton.addEventListener('mouseover', () => {
+    forwardButton.style.boxShadow ='5px 5px 5px  rgba(141, 156, 164, 0.65)';
+});
+
+forwardButton.addEventListener('mouseout', () => {
+    forwardButton.style.boxShadow ='none';
+});
+
+resButton.addEventListener('mouseover', () => {
+    resButton.style.boxShadow ='5px 5px 5px  rgba(141, 156, 164, 0.65)';
+});
+
+resButton.addEventListener('mouseout', () => {
+    resButton.style.boxShadow ='none';
+})
