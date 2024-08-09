7949852749d68b093dfb6287fe31253cf2cc212b
Riku Kuisma <rikukuisma@icloud.com>
2024-08-09 16:17:20 +0300

    ...

---
+++ rikser/src/file.js
@@ -0,0 +1,6 @@
+
+let empty = "";
+
+for (let i = 1; i <= 5; i++) {
+   console.log(i)
+}
--- rikser/src/script.js
+++ rikser/src/script.js
@@ -3,6 +3,8 @@
 const backButton = document.getElementById('back');
 const forwardButton = document.getElementById('forward');
 const resButton = document.getElementById('refresh');
+const urlInput = document.getElementById('searchfield')
+const iframe = document-getElementById('browser-frame')
 
 backButton.addEventListener('mouseover', () => {
     backButton.style.boxShadow ='5px 5px 5px  rgba(141, 156, 164, 0.65)';
@@ -26,4 +28,78 @@ resButton.addEventListener('mouseover', () => {
 
 resButton.addEventListener('mouseout', () => {
     resButton.style.boxShadow ='none';
-})
+});
+
+
+
+
+// let historyStack = [];
+// let historyIndex = -1;
+
+// function loadPage(url) {
+//     if (url === '') return;
+//     iframe.src = url;
+//     historyStack = historyStack.slice(0, historyIndex + 1);
+//     historyStack.push(url);
+//     historyIndex++;
+// }
+
+// urlInput.addEventListener('keypress', (e) => {
+//     if (e.key === 'Enter') {
+//         loadPage(urlInput.value);
+//     }
+// });
+
+// backButton.addEventListener('click', () => {
+//     if (historyIndex > 0) {
+//         historyIndex--;
+//         iframe.src = historyStack[historyIndex];
+//     }
+// });
+
+// forwardButton.addEventListener('click', () => {
+//     if (historyIndex < historyStack.length - 1) {
+//         historyIndex++;
+//         iframe.src = historyStack[historyIndex];
+//     }
+// });
+
+// resButton.addEventListener('click', () => {
+//     iframe.src = iframe.src;
+// });
+
+
+let historyStack = [];
+let historyIndex = -1;
+
+function loadPage(url) {
+    if (url === '') return;
+    iframe.src = url;
+    historyStack = historyStack.slice(0, historyIndex + 1);
+    historyStack.push(url);
+    historyIndex++;
+}
+
+urlInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        loadPage(urlInput.value);
+    }
+});
+
+backButton.addEventListener('click', () => {
+    if (historyIndex > 0) {
+        historyIndex--;
+        iframe.src = historyStack[historyIndex];
+    }
+});
+
+forwardButton.addEventListener('click', () => {
+    if (historyIndex < historyStack.length - 1) {
+        historyIndex++;
+        iframe.src = historyStack[historyIndex];
+    }
+});
+
+resButton.addEventListener('click', () => {
+    iframe.src = iframe.src;
+});
